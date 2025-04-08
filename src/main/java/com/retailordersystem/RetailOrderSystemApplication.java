1d1e1e82ecb0b0ab9b9769f3f9f0ce554b36dded
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2025-04-08 12:39:46 +0530

    Added XML files

--- src/main/java/com/retailordersystem/RetailOrderSystemApplication.java
+++ src/main/java/com/retailordersystem/RetailOrderSystemApplication.java
@@ -2,9 +2,8 @@ package com.retailordersystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 
-@SpringBootApplication(exclude = KafkaAutoConfiguration.class)
+@SpringBootApplication
 public class RetailOrderSystemApplication {
 
     public static void main(String[] args) {
--- src/main/resources/db/changelog/changes/002-insert-initial-orders.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO orders (status, description) VALUES
-    ('PENDING', 'DUMMY ORDER'),
-    ('PENDING', 'DUMMY ORDER');
